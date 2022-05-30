7ebd4a941c5b6508a6966cbb8946cb9b034e97ca
Elzada Zhakypova <aikyz9527@gmail.com>
2022-05-30 11:26:44 +0300

    add half tasks

---
+++ script.js
@@ -0,0 +1,18 @@
+/* 1-Е ЗАДАНИЕ
+
+1. Простое сообщение: сохраните текстовое сообщение в переменной и выведите его в браузере */
+
+let sayHello = "Hello my first script";
+alert(sayHello);
+console.log(sayHello);
+
+/*2. Простые сообщения: сохраните сообщение в переменной и выведите это сообщение. Затем замените значение переменной другим сообщением и выведите новое сообщение*/
+let myMessage = "This message should be changed";
+myMessage = "It was changed";
+alert(myMessage);
+
+/*3. Напишите несколько строк кода, выводящих на экран ваше имя и почтовый адрес.*/
+let userName = "My name is Aya";
+let userEmail = "Aikyz.95@mail.ru";
+alert(userName);
+alert("My E-mail address is " + userEmail);
